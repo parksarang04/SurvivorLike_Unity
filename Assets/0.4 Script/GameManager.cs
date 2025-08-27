b4975a6e6e04ecff565096152ede2b74933ccc1d
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-27 21:14:10 +0900

    feat. 타일맵 재배치
    Unity 6 변경점
    기존 Tilemap Collider 2D의 Used by Composite 체크박스가 Composite Operation 드롭다운으로 바뀌었습니다. 같은 오브젝트에 Composite Collider 2D가 붙어 있어야 옵션이 보이며, Composite Operation을 None → Merge로 설정하면 타일 개별 경계가 하나의 콜라이더로 합쳐집니다(Player 이동 시 맵 타일 격자 경계에 걸리는 현상 해소).
    참고: Rigidbody2D는 Static 유지.

---
+++ Assets/0.4 Script/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;    // static = 정적으로 사용 키워드, 바로 메모리에 얹어버린다.
+    public Player player;
+
+    void Awake()
+    {
+        instance = this;                   //인스턴스 변수를 자기자신 this로 초기화
+    }   
+}
---
+++ Assets/0.4 Script/Reposition.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework.Constraints;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+            return;
+
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float diffX =Mathf.Abs(playerPos.x - myPos.x);  //Mathf.Abs : 음수도 양수로 만들어주는 절대값 함수
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
+
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if (diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);        //Translate : 지정된 값 만큼 현재 위치에서 이동
+                }
+                else if (diffX < diffY)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);        //Translate : 지정된 값 만큼 현재 위치에서 이동
+                }
+                break;
+            case "Enemy":
+
+                break;
+        }
+    }
+}
+
+
