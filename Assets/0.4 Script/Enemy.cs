cc6aed3000b536bbb71caaa8964038abd0411cb4
parksarang04 <qkrtkfkd9074@naver.com>
2025-08-28 01:35:47 +0900

    feat. 몬스터 생성 및 타일맵 재배치 수정

---
+++ Assets/0.4 Script/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D target;
+
+    bool isLive;
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {   
+        // 위치 차이 = 타겟 위치 - 나의 위치
+        Vector2 dirVec = target.position - rigid.position;  
+        Vector2 nextVec = dirVec.normalized * speed * Time.deltaTime;
+        rigid.MovePosition(rigid.position + nextVec);   //플레이어의 키입력 값을 더한 이동 = 몬스터의 방향 값을 더한 이동
+        rigid.linearVelocity  = Vector2.zero;
+    }
+}
