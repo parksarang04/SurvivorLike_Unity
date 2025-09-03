fdd3a5d551b1f25299e20dc6618603f08bc052ee
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-03 20:42:41 +0900

    feat. 몬스터 속도

--- Assets/0.4 Script/Enemy.cs
+++ Assets/0.4 Script/Enemy.cs
@@ -8,7 +8,7 @@ public class Enemy : MonoBehaviour
     public float speed;
     public Rigidbody2D target;
 
-    bool isLive;
+    bool isLive = true;
     Rigidbody2D rigid;
     SpriteRenderer spriter;
 
@@ -19,11 +19,22 @@ public class Enemy : MonoBehaviour
     }
 
     void FixedUpdate()
-    {   
+    {
+        if (!isLive)
+            return;
+
         // 위치 차이 = 타겟 위치 - 나의 위치
         Vector2 dirVec = target.position - rigid.position;  
         Vector2 nextVec = dirVec.normalized * speed * Time.deltaTime;
         rigid.MovePosition(rigid.position + nextVec);   //플레이어의 키입력 값을 더한 이동 = 몬스터의 방향 값을 더한 이동
         rigid.linearVelocity  = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        if (!isLive)
+            return;
+
+        spriter.flipX = target.position.x < rigid.position.x;
     }
 }
