465a48a33d4a17cb8a08a16a541bf5e9276ce286
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-03 22:24:13 +0900

    feat. 오브젝트 풀링. 플레이어 따라 다니는 몬스터 위치 랜덤 생성

--- Assets/0.4 Script/GameManager.cs
+++ Assets/0.4 Script/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;    // static = 정적으로 사용 키워드, 바로 메모리에 얹어버린다.
+    public PoolManager pool;
     public Player player;
 
     void Awake()
---
+++ Assets/0.4 Script/Spqwner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spqwner : MonoBehaviour
+{
+    public Transform[] spawnPoint;
+
+    float timer;
+
+    void Awake()
+    {
+        spawnPoint = GetComponentsInChildren<Transform>();    
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 0.2f)
+        {        
+            timer = 0;
+            Spawn();
+        }
+
+    }
+
+    void Spawn()
+    {
+        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+        enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+    }
+}
