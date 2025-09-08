178a52d217dbbe429a2c6c229444da1cc5877cb9
parksarang04 <qkrtkfkd9074@naver.com>
2025-09-08 17:43:08 +0900

    study : 레벨링 시스템 적용하기

--- Assets/0.4 Script/GameManager.cs
+++ Assets/0.4 Script/GameManager.cs
@@ -5,11 +5,25 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;    // static = 정적으로 사용 키워드, 바로 메모리에 얹어버린다.
+
+    public float gameTime;
+    public float maxGameTime = 2 * 10f;
     public PoolManager pool;
     public Player player;
 
     void Awake()
     {
         instance = this;                   //인스턴스 변수를 자기자신 this로 초기화
-    }   
+    }
+
+    void Update()
+    {
+        gameTime += Time.deltaTime;
+
+        if (gameTime > maxGameTime)
+        {
+            gameTime = maxGameTime;
+        }
+
+    }
 }
--- Assets/0.4 Script/Spqwner.cs
+++ Assets/0.4 Script/Spqwner.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class Spqwner : MonoBehaviour
 {
     public Transform[] spawnPoint;
+    public SpawnData[] spawnData;  //만든 클래스를 그대로 타입으로 활용하여 배열 변수 선언
 
+    int level;
     float timer;
 
     void Awake()
@@ -17,8 +19,10 @@ public class Spqwner : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
+        level = (Mathf.Min(Mathf.FloorToInt(GameManager.instance.gameTime / 10f), spawnData.Length -1));     //적절한 숫자로 나누어 시간에 맞춰 레벨이 올라가도록 작성
+        // Mathf.FloorToInt = 소수점 아래는 버리고 int형으로 바꾸는 함수
 
-        if (timer > 0.2f)
+        if (timer > spawnData[level].spawnTime)
         {        
             timer = 0;
             Spawn();
@@ -28,7 +32,17 @@ public class Spqwner : MonoBehaviour
 
     void Spawn()
     {
-        GameObject enemy = GameManager.instance.pool.Get(Random.Range(0, 2));
+        GameObject enemy = GameManager.instance.pool.Get(0);
         enemy.transform.position = spawnPoint[Random.Range(1, spawnPoint.Length)].position;
+        enemy.GetComponent<Enemy>().Init(spawnData[level]);
     }
+}
+
+[System.Serializable]
+public class SpawnData  //SpawnData클래스는 직렬화가 되면서 unity 화면에 인스펙터 창에서 보일 수 있다.
+{
+    public float spawnTime;
+    public int spriteType;
+    public int health;
+    public float speed;
 }
